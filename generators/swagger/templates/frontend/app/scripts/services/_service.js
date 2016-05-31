450600dc675455e7e887db6d108e3bd00de5fd8c
Paulo Gladson Ximenes Pinheiro <paulo.pinheiro@serpro.gov.br>
2016-05-31 10:10:49 -0300

    Ajustes e correções

--- generators/swagger/templates/frontend/app/scripts/services/_service.js
+++ generators/swagger/templates/frontend/app/scripts/services/_service.js
@@ -1,19 +1,46 @@
 'use strict';
 
-app.factory('<%=props.caminho.split("/")[1].charAt(0).toUpperCase() + props.caminho.split("/")[1].slice(1)%>Service', ['$http', function ($http) {
+app.factory('<%=name%>Service', ['$http', function ($http) {
         var service = {};
 
-        <%props.propriedades.forEach(function (item) {%>
-
-            service.save = function (<%=entityName.toLowerCase()%>) {
-            return $http({
-                url: 'api/<%=item.caminho%>',
-                method: <%=entityName.toLowerCase()%>.id ? "PUT" : "POST",
-                data: <%=entityName.toLowerCase()%>
-            }).then();
+        service.get = function (id) {
+            return $http.get('api/<%=name.toLowerCase()%>/' + id).then(function (res) {
+                return res;
+            });
         };
 
-        <% }) %>
+        service.delete = function (id) {
+            return $http.delete('api/<%=name.toLowerCase()%>/' + id).then(function (res) {
+                return res;
+            });
+        };
+
+        service.save = function (<%=name.toLowerCase()%>) {
+            return $http({
+                url: 'api/<%=name.toLowerCase()%>',
+                method: <%=name.toLowerCase()%>.id ? "PUT" : "POST",
+                data: <%=name.toLowerCase()%>
+            }).then(
+                function (res) {
+                    return res;
+                }
+            );
+        };
+
+        service.list = function (field, order, init, qtde) {
+            return $http.get('api/<%=name.toLowerCase()%>/' + field + '/' + order + '/' + init + '/' + qtde).then(
+                function (res) {
+                    return res;
+                }
+            );
+
+        };
+
+        service.count = function () {
+            return $http.get('api/<%=name.toLowerCase()%>/count').then(function (res) {
+                return res;
+            });
+        };
 
         return service;
     }]);
