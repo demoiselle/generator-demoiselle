41b8dc32e03a503fc6038d7bc9c3770cc70967b3
Leonardo Merlin <leonardo.merlin@serpro.gov.br>
2017-01-05 16:52:23 -0200

    test: remove código desatualizado

--- test/app.js
+++ test/app.js
@@ -3,16 +3,18 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-demoiselle:app', function () {
-  before(function () {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
-      .toPromise();
-  });
+describe('generator-demoiselle:app', () => {
+  describe('when generate a Todo project', () => {
+    before(function () {
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({someAnswer: true})
+        .toPromise();
+    });
 
-  it('creates files', function () {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+    it('create files', function () {
+      assert.file([
+        'dummyfile.txt'
+      ]);
+    });
   });
 });
