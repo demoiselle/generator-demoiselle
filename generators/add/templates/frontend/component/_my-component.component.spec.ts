ade1d57367c3a057e4e7dfe92862f36e2215a718
Leonardo Merlin <leonardo.merlin@serpro.gov.br>
2017-01-05 16:54:41 -0200

    pequenos ajustes no gerado de componentes e entidades

--- generators/add/templates/frontend/component/_my-component.component.spec.ts
+++ generators/add/templates/frontend/component/_my-component.component.spec.ts
@@ -2,7 +2,6 @@
  * Testing a simple Angular 2 component
  * More info: https://angular.io/docs/ts/latest/guide/testing.html#!#simple-component-test
  */
-
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
@@ -10,7 +9,7 @@ import { DebugElement } from '@angular/core';
 
 import { <%= name.capital %>Component } from './<%= name.kebab %>.component';
 
-describe('<%= name.capital %>Component', () => {
+describe('Unit Test - <%= name.capital %>Component', () => {
     let fixture, comp, el;
 
     beforeEach(() => {
