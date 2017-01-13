d4857eeb73e19accc8af7fed475d01ca405eb6dc
Leonardo Merlin <leonardo.merlin@serpro.gov.br>
2017-01-13 13:55:58 -0200

    test: altera as descrições para pt-br

--- test/app.js
+++ test/app.js
@@ -2,18 +2,28 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-describe('generator-demoiselle:app', () => {
-  describe('when generate a Todo project', () => {
-    before(function () {
+describe('yo demoiselle:app', () => {
+  // describe('quando gerar um projeto Blank', () => {});
+  describe('quando gerar um projeto ToDo', () => {
+    before(() => {
       return helpers.run(path.join(__dirname, '../generators/app'))
-        .withPrompts({someAnswer: true})
+        .withPrompts({ someAnswer: true })
         .toPromise();
     });
 
-    it('create files', function () {
+    it('DEVE criar os arquivos', () => {
       assert.file([
         'dummyfile.txt'
       ]);
     });
+
+    it('DEVE passar nos testes', () => {
+      // assert.file(['dummyfile.txt']);
+    });
+
+    it('DEVE compilar para produção', () => {
+      // assert.file(['dummyfile.txt']);
+    });
   });
+  // describe('quando gerar um projeto Store', () => {});
 });
