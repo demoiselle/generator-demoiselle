e50abf759c9a1bb7e2554b86ca40e96f2fc7d6e5
PauloGladson <paulo.pinheiro@serpro.gov.br>
2017-02-07 15:44:47 -0200

    Backend v3

--- Utils/templates/base/backend/src/main/java/app/service/UserREST.java
+++ Utils/templates/base/backend/src/main/java/app/service/UserREST.java
@@ -2,11 +2,21 @@ package <%= package.lower %>.service;
 
 import <%= package.lower %>.entity.User;
 import io.swagger.annotations.Api;
+import javax.transaction.Transactional;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import org.demoiselle.jee.core.api.crud.Result;
 import org.demoiselle.jee.crud.AbstractREST;
 
 @Api("User")
 @Path("user")
 public class UserREST extends AbstractREST<User, String> {
+    
+    @GET
+    @Override
+    @Transactional
+    public Result find() {
+        return bc.find();
+    }
 
 }
