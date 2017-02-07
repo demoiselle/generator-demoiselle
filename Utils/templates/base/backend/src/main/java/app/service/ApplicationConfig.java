d36c5460bed183727b42e2059433ed72965d9acc
PauloGladson <paulo.pinheiro@serpro.gov.br>
2017-02-07 11:23:43 -0200

    Backend v3

--- Utils/templates/base/backend/src/main/java/app/service/ApplicationConfig.java
+++ Utils/templates/base/backend/src/main/java/app/service/ApplicationConfig.java
@@ -9,8 +9,14 @@ public class ApplicationConfig extends Application {
 
     public ApplicationConfig() {
         BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.0.0");
         beanConfig.setBasePath("/api");
+        beanConfig.setContact("demoiselle.framework@gmail.com");
+        beanConfig.setDescription("Sistema base gerado pelo generetor-demoiselle https://github.com/demoiselle/generator-demoiselle ");
+        beanConfig.setHost("demoiselle.io");
+        beanConfig.setLicense("LGPL v2");
+        beanConfig.setVersion("3.0.0");
+        beanConfig.setTermsOfServiceUrl("https://demoiselle.gitbooks.io/documentacao-jee/");
+        beanConfig.setTitle("generetor-demoiselle");
         beanConfig.setResourcePackage("<%= package.lower %>.service");
         beanConfig.setScan(true);
     }
