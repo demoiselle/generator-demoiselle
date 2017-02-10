20748c4db1e229a537697d6756e1fd2fb67bcdf5
Marcelo Teixeira <marcelo.teixeira@serpro.gov.br>
2017-02-10 09:43:50 -0200

    Adiciona entity em persistence.xml

--- Utils/backend.js
+++ Utils/backend.js
@@ -1,11 +1,14 @@
 const Util = require('./util');
 const _ = require('lodash');
 const path = require('path');
+const cheerio = require('cheerio');
 
 module.exports = class BackendUtil {
 
     constructor(vm) {
         this.util = new Util(vm);
+        this.vm = vm;
+        this.fs = vm.fs;
     }
 
     createCrud(entity, config) {
@@ -31,6 +34,9 @@ module.exports = class BackendUtil {
 
             this.util.copyTpl(from, to, template);
         });
+
+        // Adicionar imports no app.module.ts
+        this._addEntityToPersistenceXml(template);
     }
 
     createFromEntity(entity, config) {
@@ -53,5 +59,30 @@ module.exports = class BackendUtil {
 
             this.util.copyTpl(from, to, template);
         });
+
+        // Adicionar imports no app.module.ts
+        this._addEntityToPersistenceXml(template);
+    }
+
+    _addEntityToPersistenceXml(template) {
+        var templatePath = this.vm.destinationPath('backend/src/main/resources/META-INF/persistence.xml');
+        this.fs.copy(templatePath, templatePath, {
+            process: function (content) {
+                let puName = template.project.lower + 'PU';
+                let xml = cheerio.load(content.toString(), {
+                    xmlMode: true
+                });
+                let node = xml('persistence-unit[name='+ puName +']');
+                if (node.length < 1) { // if not found the app PU, get the first one
+                    node = xml('persistence-unit');
+                }
+
+                let lastClassNode = xml(node).find('class').last();
+                lastClassNode.after('\n\t<class>' + template.package.lower + '.' + template.project.lower + '.entity.' + template.name.capital + '</class>');
+
+                return xml.html();
+            }
+        });
+        this.fs.commit(function () { });
     }
 };
