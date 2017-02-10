58b120c4419609630820e315761c562827d56032
Jeferson Jose de Miranda <jeferson.miranda@serpro.gov.br>
2017-02-10 18:07:30 -0200

    Correções no Editar/Detalhes de usuário Ainda falta corrigir o combobox com os perfis

--- Utils/templates/base/frontend/src/app/user/user-edit.component.ts
+++ Utils/templates/base/frontend/src/app/user/user-edit.component.ts
@@ -10,10 +10,18 @@ import { User } from './user.model';
   selector: '<%= prefix.kebab %>-user-edit',
   templateUrl: './user-edit.component.html'
 })
-export class UserEditComponent {
+export class UserEditComponent implements OnInit {
   user: User = new User();
   id: number;
+  params: any;
   userLoaded: boolean = false;
+
+  public selectedRole;
+  public roles = [
+    {value: 'USUARIO', description: 'Usuário'},
+    {value: 'GERENTE', description: 'Gerente'},
+    {value: 'ADMINISTRADOR', description: 'Administrador'}
+  ];
 
   private routeSubscribe: any;
 
@@ -24,17 +32,46 @@ export class UserEditComponent {
     private loginService: LoginService,
     private notificationService: NotificationService)
   {
+    this.user.id = null;
     this.user.firstName = 'Admin';
     this.user.perfil = 'ADMINISTRADOR';
     this.user.email = 'admin@demoiselle.org';
     this.user.pass = '12345678';
+    this.selectedRole = this.user.perfil;
+  }
+
+  ngOnInit() {
+    this.loadPerfil();
+    this.route.params.subscribe(params => {
+      console.log(params);
+      if (Object.keys(params).length > 0) {
+        this.user = <User> params;
+        this.selectedRole = this.user.perfil;
+      }
+    });
+  }
+
+  loadPerfil() {
+    this.service.getPerfil().subscribe(
+      (result) => {
+        this.roles = result;
+        if (this.roles.length > 0) {
+          this.selectedRole = this.roles[0];
+        }
+      },
+      (error) => {
+        console.log('error');
+        console.log(error);
+        this.notificationService.error('Não foi possível carregar a lista de perfis!');
+      }
+    );
   }
 
   loadUsuario() {
     this.service.get(this.id)
       .subscribe(
-      (usuario: User) => {
-        this.user = usuario;
+      (user: User) => {
+        this.user = user;
         this.userLoaded = true;
       },
       error => {
@@ -44,19 +81,35 @@ export class UserEditComponent {
   }
 
   save(user:User) {
-    this.service.create(user).subscribe(
-      (result) => {
-        this.notificationService.success('Usuário criado com sucesso!');
-        this.goBack();
-      },
-      (error) => {
-        this.notificationService.error('Não foi possível salvar o usuário!');
-      }
-    );
+    if (user.id) {
+      this.service.create(user).subscribe(
+        (result) => {
+          this.notificationService.success('Usuário criado com sucesso!');
+          this.goBack();
+        },
+        (error) => {
+          this.notificationService.error('Não foi possível salvar o usuário!');
+        }
+      );
+    }
+    else {
+      this.service.update(user).subscribe(
+        (result) => {
+          this.notificationService.success('Usuário alterado com sucesso!');
+          this.goBack();
+        },
+        (error) => {
+          this.notificationService.error('Não foi possível alterar o usuário!');
+        }
+      );
+    }
   }
   
   goBack() {
     this.router.navigate(['user']);
   }
 
+  changeRole(event) {
+    this.selectedRole = event.target.value;
+  }
 }
