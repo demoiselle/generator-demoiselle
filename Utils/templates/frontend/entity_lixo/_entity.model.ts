3636c691c0e423d608995ad7bb1090ce45598cd4
PauloGladson <paulo.pinheiro@serpro.gov.br>
2017-02-10 13:29:34 -0200

    Retirada dos códigos desnecessários

--- Utils/templates/frontend/entity_lixo/_entity.model.ts
+++
@@ -1,8 +0,0 @@
-export class <%= name.capital %> {
-  id: number;
-  description: string;
-  constructor(
-    id?: number,
-    description?: string
-  ) { }
-}
--- Utils/templates/frontend/entity_lixo/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './<%= name.kebab %>.component';
-export * from './<%= name.kebab %>.module';
