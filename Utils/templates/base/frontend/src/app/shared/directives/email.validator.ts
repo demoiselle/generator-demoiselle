643318df741a266d3bb7636730dd6fb275dfe24d
Jeferson Jose de Miranda <jeferson.miranda@serpro.gov.br>
2017-03-06 10:25:00 -0300

    Correções nos validadores

--- Utils/templates/base/frontend/src/app/shared/directives/email.validator.ts
+++ Utils/templates/base/frontend/src/app/shared/directives/email.validator.ts
@@ -1,18 +1,6 @@
 import { Directive, forwardRef } from '@angular/core';
 import { NG_VALIDATORS, FormControl } from '@angular/forms';
 
-function validateEmailFactory() {
-  return (c: FormControl) => {
-    let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-
-    return EMAIL_REGEXP.test(c.value) ? null : {
-      validateEmail: {
-        valid: false
-      }
-    };
-  };
-}
-
 @Directive({
   selector: '[validateEmail][ngModel],[validateEmail][formControl]',
   providers: [
@@ -20,14 +8,14 @@ function validateEmailFactory() {
   ]
 })
 export class EmailValidator {
-
-  validator: Function;
-
-  constructor() {
-    this.validator = validateEmailFactory();
-  }
-
+  
+  EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+  
   validate(c: FormControl) {
-    return this.validator(c);
+    return this.EMAIL_REGEXP.test(c.value) ? null : {
+      validateEmail: {
+        valid: false
+      }
+    };
   }
 }
--- Utils/templates/base/frontend/src/app/shared/directives/equal.validator.ts
+++ Utils/templates/base/frontend/src/app/shared/directives/equal.validator.ts
@@ -15,28 +15,22 @@ export class EqualValidator implements Validator {
         // self value
         let v = c.value;
 
-        // control vlaue
+        // control value
         let e = c.root.get(this.validateEqual);
 
-        // value equal and reverse
+        // value equal
         if (e && v === e.value) {
             delete e.errors['validateEqual'];
             if (!Object.keys(e.errors).length) e.setErrors(null);
         }
 
-        // value not equal and reverse
+        // value not equal
         if (e && v !== e.value) {
             e.setErrors({
                 validateEqual: true
             });
         }
-        if (e) {
-            console.log(e.value);
-            console.log(v);
-            console.log(e.errors);
-        }
 
-        console.log('E: ' + e);
         return e ? e.errors : null;
     }
 }
