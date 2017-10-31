fa18e8bea80347077926c0006281a96a2f7d4716
Marcelo Teixeira <marcelo.teixeira@serpro.gov.br>
2017-10-31 16:10:21 -0200

    Ajustes

--- Utils/templates/base/frontend/src/app/app-routing.module.ts
+++ Utils/templates/base/frontend/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserComponent } from './user';
 import { AuthGuard } from '@demoiselle/security';
 
 // Import Containers
