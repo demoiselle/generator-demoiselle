ecd0a0ba9fb2dbfb9d570a8b3552971ebb35b088
Marcelo Teixeira <marcelo.teixeira@serpro.gov.br>
2017-10-31 15:46:59 -0200

    Correção na cópia de webapps

--- generators/app/index.js
+++ generators/app/index.js
@@ -237,7 +237,7 @@ module.exports = class AppGenerator extends Generator {
 
         from = this.templatePath('base/backend/src/main/webapp/');
         to = this.destinationPath('backend/src/main/webapp/');
-        this.fs.copyTpl(from, to, template);
+        this.fs.copy(from, to);
 
         from = this.templatePath('base/backend/pom.xml');
         to = this.destinationPath('backend/pom.xml');
