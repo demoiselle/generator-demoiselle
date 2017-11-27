a0e6f72df05f898ce309c6b2cf0d6299ec011dd8
Paulo Gladson Ximenes Pinheiro <paulo.pinheiro@serpro.gov.br>
2017-11-27 11:46:44 -0200

    Ajuste do usuário inicial

--- Utils/templates/base/backend/src/main/resources/import.sql
+++ Utils/templates/base/backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
 -- You can use this file to load seed data into the database using SQL statements
 -- senha: '123456' === 'e10adc3949ba59abbe56e057f20f883e'
-INSERT INTO USER VALUES('b83810af-7ba9-4aea-8bb6-f4992a72c5fb', 'admin@demoiselle.org', 'admin', 'e10adc3949ba59abbe56e057f20f883e', 0);
+INSERT INTO USER (ID, EMAIL, FIRSTNAME, PASS, PERFIL) VALUES ('b83810af-7ba9-4aea-8bb6-f4992a72c5fb', 'admin@demoiselle.org', 'admin', 'e10adc3949ba59abbe56e057f20f883e', 0);
