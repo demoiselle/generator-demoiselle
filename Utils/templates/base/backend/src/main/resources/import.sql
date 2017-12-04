f2e787555a2478d7948fcfec715f175f9332d483
Leonardo Merlin <leonardo.merlin@serpro.gov.br>
2017-12-04 14:26:03 -0200

    add: new users to starter (import.sql)

--- Utils/templates/base/backend/src/main/resources/import.sql
+++ Utils/templates/base/backend/src/main/resources/import.sql
@@ -1,3 +1,8 @@
 -- You can use this file to load seed data into the database using SQL statements
 -- senha: '123456' === 'e10adc3949ba59abbe56e057f20f883e'
-INSERT INTO USER (ID, EMAIL, FIRSTNAME, PASS, PERFIL) VALUES ('b83810af-7ba9-4aea-8bb6-f4992a72c5fb', 'admin@demoiselle.org', 'admin', 'e10adc3949ba59abbe56e057f20f883e', 0);
+INSERT INTO USER (ID, EMAIL, FIRSTNAME, PASS, PERFIL) VALUES ('b83810af-7ba9-4aea-8bb6-f4992a72c5fb', 'admin@demoiselle.org', 'Admin', 'e10adc3949ba59abbe56e057f20f883e', 0);
+INSERT INTO USER (ID, EMAIL, FIRSTNAME, PASS, PERFIL) VALUES ('dd10b422-ecc4-4aa3-879a-cdc25ca4eff8', 'gerente_1@demoiselle.org', 'Gerente1', 'e10adc3949ba59abbe56e057f20f883e', 1);
+INSERT INTO USER (ID, EMAIL, FIRSTNAME, PASS, PERFIL) VALUES ('0f23cb4d-404b-41e7-84d7-bd9e85a8f892', 'gerente_2@demoiselle.org', 'Gerente2', 'e10adc3949ba59abbe56e057f20f883e', 1);
+INSERT INTO USER (ID, EMAIL, FIRSTNAME, PASS, PERFIL) VALUES ('538b7006-8a3b-4705-8e41-c58930bf3bc9', 'usuario_1@demoiselle.org', 'Usuário1', 'e10adc3949ba59abbe56e057f20f883e', 2);
+INSERT INTO USER (ID, EMAIL, FIRSTNAME, PASS, PERFIL) VALUES ('86403602-b7e3-4c25-81de-de17cf2c9cf2', 'usuario_2@demoiselle.org', 'Usuário2', 'e10adc3949ba59abbe56e057f20f883e', 2);
+INSERT INTO USER (ID, EMAIL, FIRSTNAME, PASS, PERFIL) VALUES ('8f492e60-5e1e-4400-9570-eb16cc6bd563', 'usuario_3@demoiselle.org', 'Usuário3', 'e10adc3949ba59abbe56e057f20f883e', 2);
